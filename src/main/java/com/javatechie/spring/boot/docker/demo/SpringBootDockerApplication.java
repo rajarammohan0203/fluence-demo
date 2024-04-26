47e8c231be4b6b7eb3390c27cc84070ef1050d6c
rajarammohan0203 <52132926+rajarammohan0203@users.noreply.github.com>
2024-04-26 13:30:03 +0530

    Update SpringBootDockerApplication.java

--- src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
+++ src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringBootDockerApplication {
 
-        @GetMapping("/fluence")
-        public String getMessage() {
-                return "<!DOCTYPE html>" +
+    @GetMapping("/fluence")
+    public String getMessage() {
+        return "<!DOCTYPE html>" +
                 "<html lang=\"en\">\n" +
                 "<head>\n" +
                 "    <meta charset=\"UTF-8\">\n" +
@@ -21,68 +21,59 @@ public class SpringBootDockerApplication {
                 "    <title>Fluence Project</title>\n" +
                 "    <style>\n" +
                 "        body {\n" +
-                "            background-color: skyblue;\n" +
+                "            background-color: white;\n" +
                 "            color: black;\n" +
                 "            margin: 0;\n" +
                 "            padding: 0;\n" +
                 "            font-family: Arial, sans-serif;\n" +
                 "        }\n" +
-                "        header, footer {\n" +
-                "            background-color: black;\n" +
+                "        header {\n" +
+                "            background-color: gray;\n" +
                 "            color: white;\n" +
-                "            padding: 30px;\n" +
-                "            text-align: center;\n" +
+                "            padding: 10px;\n" +
+                "            text-align: left;\n" +
+                "            width: 100%;\n" +
+                "            z-index: 1000;\n" +
+                "        }\n" +
+                "        .logo {\n" +
+                "            position: fixed;\n" +
+                "            top: 10px;\n" +
+                "            right: 10px;\n" +
                 "        }\n" +
                 "        .content {\n" +
                 "            text-align: center;\n" +
-                "            margin-top: 50px;\n" +
+                "            margin-top: 100px;\n" +
+                "            padding: 20px;\n" +
+                "        }\n" +
+                "        .box {\n" +
+                "            background-color: lightblue;\n" +
+                "            padding: 20px;\n" +
+                "            margin-top: 20px;\n" +
+                "            border-radius: 5px;\n" +
+                "            font-size: 24px; /* Increased font size */\n" +
                 "        }\n" +
                 "</style>\n" +
                 "</head>\n" +
                 "<body>\n" +
                 "<header>\n" +
-                "    <a href=\"#\">Products</a> |\n" +
-                "    <a href=\"#\">Sign up</a> |\n" +
-                "    <a href=\"#\">Login</a> |\n" +
-                "    <a href=\"#\">Team Size</a> |\n" +
-                "    <a href=\"#\">About us</a> |\n" +
-                "    <a href=\"#\">Contact</a>\n" +
+                "    <h1>Fluence Project</h1>\n" +
                 "</header>\n" +
+                "<div class=\"logo\">\n" +
+                "    <img src=\"./src/einfochips.png\" alt=\"Your Logo\">\n" +
+                "</div>\n" +
                 "<div class=\"content\">\n" +
                 "    <h1>Hello Team, Welcome to Fluence Project</h1>\n" +
-                "    <p>This is a DEMO Application</p>\n" +
+                "    <p>This is a DEMO 2 Application</p>\n" +
+                "    <div class=\"box\">\n" +
+                "        <h2>End to End CI/CD setup using CircleCI, JFrog, ArgoCD, and EKS</h2>\n" +
+                "    </div>\n" +
                 "</div>\n" +
                 "</body>\n" +
                 "</html>";
-        }
+    }
 
-        public static void main(String[] args) {
-                SpringApplication.run(SpringBootDockerApplication.class, args);
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
 
 }
-
-// #######  OLD CODE FOR TESTING ####################
-/*package com.javatechie.spring.boot.docker.demo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
-public class SpringBootDockerApplication {
-
-	@GetMapping("/message")
-	public String getMessage() {
-		return "Hello world!! This is Java Application";
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDockerApplication.class, args);
-	}
-
-}*/
-
-
